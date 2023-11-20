7e4995747151347097c626b9bdfdd57d29264e05
neetuchoudhary021993 <132840941+neetuchoudhary021993@users.noreply.github.com>
2023-11-20 23:38:20 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
+// const fs = require("fs/promises");
+// const fileName = "myfile.txt";
+// const reader = async (fileName) => {
+//   // write code here
+// 	// dont change function name
+  
+// };
+
+
+// module.exports =  reader ;
+
+
 const fs = require("fs/promises");
 const fileName = "myfile.txt";
-const reader = async (fileName) => {
-  // write code here
-	// dont change function name
-  
+
+const reader = async (filename) => {
+    // Read the content of the file using fs.promises.readFile
+    const content = await fs.readFile(filename, "utf-8");
+    return content;
 };
 
+module.exports = reader;
 
-module.exports =  reader ;
